e207061f15eaf58cf70e5d0387e89f46a05018e3
Greg Kolb <greg.kolb@dolby.com>
2022-10-18 10:25:54 -0600

    fix check origin for internal requests

--- main.go
+++ main.go
@@ -4,72 +4,48 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/go-chi/chi"
+	"github.com/gorilla/websocket"
 	"github.com/gregkolb/example/graph"
 	"github.com/gregkolb/example/graph/generated"
 	"github.com/rs/cors"
-	// "github.com/gorilla/websocket"
 )
 
-const defaultPort = "8080"
-
-// func main() {
-// 	port := os.Getenv("PORT")
-// 	if port == "" {
-// 		port = defaultPort
-// 	}
-
-// 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-
-// 	srv.AddTransport(&transport.Websocket{}) // <---- This is the important part!
-
-// 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-// 	http.Handle("/query", srv)
-
-// 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-// 	log.Fatal(http.ListenAndServe(":"+port, nil))
-// }
+const defaultPort = "4000"
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
-	router := chi.NewRouter()
 
-	// Add CORS middleware around every request
-	// See https://github.com/rs/cors for full option listing
-	// router.Use(cors.New(cors.Options{
-	// 	AllowedOrigins:   []string{"http://localhost:8080", "http://localhost:3000", "*"},
-	// 	AllowCredentials: true,
-	// 	Debug:            true,
-	// }).Handler)
+	// CORS setup
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:4000", "http://localhost:3000"},
+		AllowCredentials: true,
+		Debug:            false,
+	})
+	// Use New instead of NewDefaultServer in order to have full control over defining transports
+	srv := handler.New(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	})
+	srv.Use(extension.Introspection{})
 
-	router.Use(cors.Default().Handler)
-	
-    srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-	srv.AddTransport(&transport.Websocket{})
-    // srv.AddTransport(&transport.Websocket{
-    //     Upgrader: websocket.Upgrader{
-    //         CheckOrigin: func(r *http.Request) bool {
-    //             // Check against your desired domains here
-    //              return true
-    //         },
-    //         ReadBufferSize:  1024,
-    //         WriteBufferSize: 1024,
-    //     },
-    // })
-
-	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	router.Handle("/query", srv)
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", c.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	err := http.ListenAndServe(":"+port, router)
-	if err != nil {
-		panic(err)
-	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
